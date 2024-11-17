9d255d2ebef4b4a4a0b89ba2ca4a59f08e73f827
Lau <laura.leiva.cataldo@gmail.com>
2024-11-17 19:10:50 -0300

    ns

--- ProgDinamica.cpp
+++ ProgDinamica.cpp
@@ -56,8 +56,14 @@ int distancia_edicion(const string& s1, const string& s2) {
     int m = s1.length();
     int n = s2.length();
     vector<vector<int>> dp(m + 1, vector<int>(n + 1));
-    for (int i = 0; i <= m; ++i) dp[i][0] = i;
-    for (int j = 0; j <= n; ++j) dp[0][j] = j;
+    dp[0][0] = 0;
+    for (int i = 1; i <= m; ++i) {
+        dp[i][0] = dp[i-1][0] + costo_del(s1[i-1]);
+    }
+    for (int j = 1; j <= n; ++j) {
+        dp[0][j] = dp[0][j-1] + costo_ins(s2[j-1]);
+    }
+
     for (int i = 1; i <= m; ++i) {
         for (int j = 1; j <= n; ++j) {
             int substitution_cost = dp[i-1][j-1] + costo_sub(s1[i-1], s2[j-1]);
--- Resultadoss/Images/graficos.py
+++ Resultadoss/Images/graficos.py
@@ -4,11 +4,13 @@ import numpy as np
 casos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 m = [0, 8, 2, 3, 16, 8, 3, 32, 22, 12]  # Longitudes de s1
 n = [0, 4, 10, 50, 10, 7, 3, 0, 8, 12]  # Longitudes de s2
-TFuerzaBruta = [0.0009, 0.2948, 0.0387, 54.3038, 37692.0667, 0.3102, 0.0112, None, 0.0056, 17540.3667]
+TFuerzaBruta = [0.0009, 0.2948, 0.0387, 54.3038, 37692.0667, 0.3102, 0.0112, 0.0032, None, 17540.3667]
 TProgDinamica = [0.0072, 0.0273, 0.0489, 0.0293, 0.0528, 0.0932, 0.0101, 0.0239, 0.0634, 0.0448]
 
+fuerza_bruta = [133301, 133301, 133301, 133352, 133332, 133301, 133301, 133334, None, 133301]
+prog_dinamica = [133342, 133726, 133558, 134477, 134541, 133846, 133486, 134271, 135748, 134350]
 
-# Gráfico 1: Fuerza Bruta
+
 plt.figure(figsize=(10, 6))
 plt.plot(casos, TFuerzaBruta, color='red', marker='o', label='Fuerza Bruta', linestyle='-', markersize=6)
 plt.title('Costo de Transformación usando Fuerza Bruta')
@@ -18,7 +20,6 @@ plt.legend()
 plt.grid(True)
 plt.show()
 
-# Gráfico 2: Programación Dinámica
 plt.figure(figsize=(10, 6))
 plt.plot(casos, TProgDinamica, color='blue', marker='x', label='Programación Dinámica', linestyle='-', markersize=6)
 plt.title('Costo de Transformación usando Programación Dinámica')
@@ -51,3 +52,12 @@ plt.legend()
 plt.grid(True)
 plt.show()
 
+plt.figure(figsize=(10, 6))
+plt.plot(casos, fuerza_bruta, label="Fuerza Bruta", marker='o', color='b')
+plt.plot(casos, prog_dinamica, label="ProgDinamica", marker='s', color='r')
+plt.title("Comparación de Asignación de Memoria entre Fuerza Bruta y ProgDinamica")
+plt.xlabel("Casos")
+plt.ylabel("Bytes")
+plt.legend()
+plt.grid(True)
+plt.show()
